857e26df2f25db4fd5c0411a061f9dcc9e9d672e
Thiago william <thiago.will.silva@gmail.com>
2025-05-14 20:12:52 -0300

    feat: active link componet

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
-import { useRouter } from "next/router";
-import { cn } from "@/lib/utils";
+
+import { ActiveLink } from "../active-link/active-link";
 
 export const Header = () => {
-  const router = useRouter();
-  const isHomePage = router.pathname === "/";
-  const isBlogPage = router.pathname === "/blog";
+
   return (
     <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filter]:bgbackground/60">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-80">
@@ -14,23 +12,11 @@ export const Header = () => {
           <Link href="/">Logo</Link>
 
           <nav className="flex items-center gap-6">
-            <Link 
-              href="/" 
-              className={ cn(
-                'text-sm font-medium transition-colors hover:text-blue-500', 
-                isHomePage ? 'text-blue-500':'text-muted-foreground')}
-            >
-              Início
-            </Link>
-            <Link 
-              href="/blog"
-              className={ cn(
-                'text-sm font-medium transition-colors hover:text-blue-500', 
-                isBlogPage ? 'text-blue-500':'text-muted-foreground')}
-            >
-              Blog
-            </Link>
-            <Button variant="secondary">Começar</Button>
+            <ActiveLink href="/"> Início </ActiveLink>
+            <ActiveLink href="/blog"> Blog </ActiveLink>
+            <Button variant="secondary" asChild>
+              <Link href="/comecar"> Começar </Link>
+            </Button>
           </nav>
         </div>
       </div>
