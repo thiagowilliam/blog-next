f820d1ca3b285168be8726f5bb5456f6566df304
Thiago william <thiago.will.silva@gmail.com>
2025-05-26 20:50:20 -0300

    feat: section call to action and ajustment fonts

---
+++ src/components/call-to-action/call-to-action.tsx
@@ -0,0 +1,29 @@
+import { ArrowRight, Store } from 'lucide-react';
+import Link from 'next/link';
+import { Button } from '../ui/button';
+
+
+export const CallToAction = () => {
+  return (
+    <section className="py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
+      <div className="container">
+        <div className="flex flex-col items-center gap-6 text-center">
+          <div className="p-4 bg-cyan-300 w-fit rounded-full">
+            <Store className="text-cyan-100" />
+          </div>
+
+           <h2 className="font-sans text-gray-100 text-balance text-heading-xl">
+            Crie uma loja online e inicie suas vendas ainda hoje
+          </h2>
+
+          <Button variant="primary" asChild className="mt-6">
+            <Link href="/criar-loja">
+              Criar loja grátis
+              <ArrowRight />
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/components/feature-section/feature-section.tsx
+++ src/components/feature-section/feature-section.tsx
@@ -11,7 +11,7 @@ export const FeatureSection = () => {
         <span className="text-body-tag text-blue-200 bg-blue-400 px-2 py-1 w-fit rounded-sm uppercase">
           Simples
         </span>
-        <h2 className="text-gray-100 text-heading-lg">
+        <h2 className="font-sans text-gray-100 text-heading-lg">
           Crie um catálogo de produtos online em poucos minutos
         </h2>
       </div>
@@ -20,7 +20,7 @@ export const FeatureSection = () => {
         <span className="text-body-tag text-blue-200 bg-blue-400 px-2 py-1 w-fit rounded-sm uppercase">
           Prático
         </span>
-        <h2 className="text-gray-100 text-heading-lg">
+        <h2 className="font-sans text-gray-100 text-heading-lg">
           Venda para seu público através de uma plataforma única
         </h2>
       </div>
@@ -32,7 +32,7 @@ export const FeatureSection = () => {
               Personalizável
             </span>
 
-            <h2 className="text-gray-100 text-heading-lg">
+            <h2 className="font-sans text-gray-100 text-heading-lg">
               Tenha uma loja online personalizada com a cara da sua marca
             </h2>
 
